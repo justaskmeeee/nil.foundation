5979800404e5d4fcece106429c02223c27e4d408
ukorvl <ukorotovskiy@gmail.com>
2022-09-14 17:08:06 +0400

    app _app with global layout #1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
+import { Layout } from '@nilfoundation/react-components';
 import type { AppProps } from 'next/app'
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
+/**
+ * Custom App. Provides global styles and layout for all pages.
+ *
+ * @param {AppProps} props - Props.
+ * @returns - React component.
+ */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <Layout
+            navbar={<Header />}
+            footer={<Footer />}
+        >
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default MyApp
