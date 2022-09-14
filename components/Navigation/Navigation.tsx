aba4c0075125ff1e9ce824c4bdaf1c53d01c5734
ukorvl <ukorotovskiy@gmail.com>
2022-09-14 18:01:20 +0400

    rename navigationLinks to navigationSettings #1

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,15 +1,15 @@
 import { NavList } from '@nilfoundation/react-components';
-import navigationLinks from './navigationLinks';
+import navigationSettings from './navigationSettings';
 
 /**
  * Navigation component.
- * 
- * @returns React component. 
+ *
+ * @returns React component.
  */
 const Navigation = (): JSX.Element =>
     <NavList className="navbar-right navbar-nav">
         {
-            navigationLinks.map(({title, path}) =>
+            navigationSettings.map(({title, path}) =>
                 <NavList.Link
                     key={title}
                     href={path}
