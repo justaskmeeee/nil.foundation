8304f048023f7cf0bf060d0941c19a3c687d6ae7
ukorvl <ukorotovskiy@gmail.com>
2022-09-14 17:08:25 +0400

    create footer #1

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import { Col, Container, Row } from '@nilfoundation/react-components';
+
+/**
+ * Footer component.
+ *
+ * @returns React component.
+ */
+const Footer = (): JSX.Element =>
+    <footer>
+        <div className="footer-below">
+            <Container>
+                <Row>
+                    <Col className="text-center">
+                        {`Copyright © =nil; Foundation ${ new Date().getFullYear() }`}
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    </footer>
+
+export default Footer;
