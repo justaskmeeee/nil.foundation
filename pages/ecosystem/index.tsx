487047754dff967269623016e82811a15c7075f3
ukorvl <ukorotovskiy@gmail.com>
2022-09-25 23:09:31 +0400

    contact form added #1

---
+++ pages/ecosystem/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next';
+import { Col, Container, Row } from '@nilfoundation/react-components';
+import Head from 'next/head';
+
+/**
+ * Index page.
+ *
+ * @returns NextPage.
+ */
+const IndexPage: NextPage = () => {
+    return (
+        <main>
+            <Head>
+                <title>=nil; Foundation</title>
+            </Head>
+            djjdjddjjdjdj
+        </main>
+    );
+};
+
+export default IndexPage;
