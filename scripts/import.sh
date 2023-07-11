c3c62b46c961854a88d72a8d4a5e01d0ad837578
KlonD90 <newbiecraft@gmail.com>
2023-07-11 14:36:41 +0700

    add import

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 git clone --depth=1 --branch=master git@github.com:NilFoundation/blog.nil.foundation.git blog
+set -e
 cd blog
 rm -rf .git
 docker build . -t blognilfoundation
-docker run -v `pwd`:/srv/jekyll blognilfoundation  jekkyl build
+docker run -v `pwd`:/srv/jekyll blognilfoundation  jekyll build
+cd ..
 node import_old_post.js
--- scripts/import_old_post.js
+++ scripts/import_old_post.js
@@ -4,29 +4,62 @@ var fs = require('fs');
 var path = require('path');
 const cheerio = require('cheerio');
 const axios = require('axios')
+const qs = require('qs')
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 var siteDir = path.join(__dirname, 'blog/_site/');
 const client = axios.create({
   baseURL: process.env.API_URL,
-  Headers: {
-    'Authorization': 'beare ' + process.env.API_TOKEN,
-  }
+})
+client.interceptors.request.use((config) => {
+  config.headers['Authorization'] = `Bearer ${process.env.API_TOKEN}`
+  return config
 })
 
-const addTagIfNot = async (tag) => {
-  const tags = await client.get('/api/tags')
 
+const addTagIfNot = async (tag) => {
+  const urlParams = qs.stringify(
+    {
+      filters: { slug: tag },
+    }
+  )
+  const tags = await client.get(`/api/tags?${urlParams}`)
+  if (tags.data.data.length > 0) {
+    return tags.data.data[0].id
+  }
+  const newTag = await client.post('/api/tags', {
+    data: {
+      name: tag,
+      slug: tag
+    }
+  })
+  return newTag.data.data.id
+}
+
+const processPosts = async (posts) => {
+  for (const content of posts) {
+    await uploadPost(content.title, content.date, content.tags, content.author, content.slug, content.body)
+  }
 }
 
 const uploadPost = async (title, date, tags, author, slug, body) => {
   const splittedTags = tags.split(' ').map(x => x.trim()).filter(x => x.length > 0)
-  await client.post('/api/blog', {
+  const tagIds = []
+  for (const tag of splittedTags) {
+    const tagId = await addTagIfNot(tag)
+    tagIds.push(tagId)
+  }
+  await client.post('/api/blogs', {
     data: {
       title,
       date,
       author,
       slug,
-      body
+      content: body,
+      tags: tagIds
     }
   })
 
@@ -45,9 +78,10 @@ const readMd = (fileName) => {
   }
   return fs.readFileSync(path.join(postsDir, foundFile), 'utf8')
 }
-
-const prefixPath = ['2021', '2022', '2023']
-for (const year of prefixPath) {
+const addContent = []
+const siteDirs = fs.readdirSync(siteDir)
+const yearDirs = siteDirs.filter(x => /^[0-9]{4}$/.test(x))
+for (const year of yearDirs) {
   let curYearDir = path.join(siteDir, year);
   const monthsDir = fs.readdirSync(curYearDir)
   for (const month of monthsDir) {
@@ -69,9 +103,24 @@ for (const year of prefixPath) {
         const title = md.match(/title: (.*)/)[1]
         const tags = md.match(/tags: (.*)/)[1]
         const author = md.match(/author: (.*)/)[1]
-        const slug = curFile.split('.')[0]
-        console.log(body, 'title', title, 'date', date, 'tags', tags, 'author', author, 'slug', slug)
+        const slug = file.split('.')[0]
+        console.log('slug', slug)
+        addContent.push({
+          title,
+          date,
+          tags,
+          author,
+          slug,
+          body
+        })
       }
     }
   }
 }
+
+processPosts(addContent).then(() => {
+  process.exit(0)
+}).catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
