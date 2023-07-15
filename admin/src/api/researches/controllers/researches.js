855f1258a32fe00d9bc7a74ae267623daa05ee0c
root <newbiecraft@gmail.com>
2023-07-15 09:23:05 +0000

    change scheme

---
+++ admin/src/api/researches/controllers/researches.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * researches controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::researches.researches');
