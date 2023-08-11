b78182d9bfc598ebd91be842d64f19cecddfb65c
KlonD90 <newbiecraft@gmail.com>
2023-08-11 10:44:41 +0700

    default config

--- site/src/strapi/getSiteConfig.ts
+++ site/src/strapi/getSiteConfig.ts
@@ -5,11 +5,19 @@ type SiteConfig = {
   isReserachTagsOn: boolean;
 };
 
+const defaultConfig = {
+  isGlossaryOn: false,
+  isReserachTagsOn: false,
+}
+
 export const getSiteConfig = async (): Promise<SiteConfig> => {
-  const res = await client.get<{data?: {attributes: SiteConfig}}>('/config');
-
-  if (!res?.data?.data?.attributes) throw new Error('No config found');
-
-  return res.data.data.attributes;
+  try { 
+    const res = await client.get<{data?: {attributes: SiteConfig}}>('/config');
+    if (!res?.data?.data?.attributes) throw new Error('No config found');
+    return res.data.data.attributes;
+  } catch (e) {
+    console.error(e);
+    return defaultConfig;
+  }
 };
 
