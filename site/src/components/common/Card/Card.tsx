c2d0265b83fcfb1d29f58abc7cdf7e0e16bd6192
Ivan Tavruev <ivan.tavruev@saltpepper.ru>
2023-10-30 12:57:08 +0300

    formatter fixes

--- site/src/components/common/Card/Card.tsx
+++ site/src/components/common/Card/Card.tsx
@@ -1,32 +1,25 @@
-import Button from 'components/Button';
-import Icon from 'components/Icon';
-import React from 'react';
+import Button from 'components/Button'
+import Icon from 'components/Icon'
+import React from 'react'
 
-import s from './Card.module.scss';
+import s from './Card.module.scss'
 
-import cx from 'classnames';
+import cx from 'classnames'
 
 type CardProps = {
-  children?: React.ReactNode;
-  href?: string;
-  onClick?: (e: MouseEvent) => void;
-  className?: string;
-};
+  children?: React.ReactNode
+  href?: string
+  onClick?: (e: MouseEvent) => void
+  className?: string
+}
 
 const Card = ({ children, href, onClick, className }: CardProps) => {
   return (
-    <Button
-      href={href}
-      onClick={onClick}
-      className={cx(s.root, className)}
-    >
+    <Button href={href} onClick={onClick} className={cx(s.root, className)}>
       {children}
-      <Icon
-        className={s.arrow}
-        name="arrow-up"
-      />
+      <Icon className={s.arrow} name="arrow-up" />
     </Button>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
